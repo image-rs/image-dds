0d870c12a406aeade637d5eedd2c3e8df888a409
RunDevelopment <mitchi5000.ms@googlemail.com>
2025-03-24 13:12:03 +0100

    Add docs for `Format`

--- src/format.rs
+++ src/format.rs
@@ -6,6 +6,10 @@ use crate::{
     FormatError, Precision, SizeMultiple,
 };
 
+/// The format of the pixel data of a surface.
+///
+/// This enumeration is modelled after the [DXGI_FORMAT enumeration](https://learn.microsoft.com/en-us/windows/win32/api/dxgiformat/ne-dxgiformat-dxgi_format)
+/// and has the same semantics and naming conventions.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 #[non_exhaustive]
 #[allow(non_camel_case_types)]
