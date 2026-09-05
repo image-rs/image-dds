8cae213935871afd96c6d194a7d56555d8a01ee4
Michael Schmidt <msrd0000@gmail.com>
2026-09-05 23:50:02 +0200

    Gamma correction for mipmap generation (#120)

--- src/resize.rs
+++ src/resize.rs
@@ -126,6 +126,7 @@ impl ResizeState {
         src: AlignedView,
         new_size: Size,
         straight_alpha: bool,
+        gamma_correction: bool,
         filter: ResizeFilter,
     ) -> AlignedView<'a> {
         let color = src.color();
@@ -133,7 +134,14 @@ impl ResizeState {
         // prepare the destination buffer
         let dest_slice = get_aligned_slice(&mut self.dest_buffer, new_size, color);
 
-        resize_into(src, dest_slice, new_size, straight_alpha, filter);
+        resize_into(
+            src,
+            dest_slice,
+            new_size,
+            straight_alpha,
+            gamma_correction,
+            filter,
+        );
 
         AlignedView::new(dest_slice, new_size, color)
     }
@@ -143,6 +151,7 @@ pub(crate) fn resize(
     src: AlignedView,
     new_size: Size,
     straight_alpha: bool,
+    gamma_correction: bool,
     filter: ResizeFilter,
 ) -> AlignedBuffer {
     let color = src.color();
@@ -151,7 +160,14 @@ pub(crate) fn resize(
     let mut dest_buffer: Vec<AlignTo> = Vec::new();
     let dest_slice = get_aligned_slice(&mut dest_buffer, new_size, color);
 
-    resize_into(src, dest_slice, new_size, straight_alpha, filter);
+    resize_into(
+        src,
+        dest_slice,
+        new_size,
+        straight_alpha,
+        gamma_correction,
+        filter,
+    );
 
     AlignedBuffer::new(dest_buffer, new_size, color)
 }
@@ -178,6 +194,7 @@ fn resize_into(
     dest_slice: &mut [u8],
     new_size: Size,
     straight_alpha: bool,
+    gamma_correction: bool,
     filter: ResizeFilter,
 ) {
     let color = src.color();
@@ -199,27 +216,35 @@ fn resize_into(
         filter,
     };
 
-    use Channels as C;
-    use Precision as P;
+    fn dispatch<G: GammaCorrection>(args: Args, color: ColorFormat, straight_alpha: bool) {
+        use Channels as C;
+        use Precision as P;
 
-    if straight_alpha && color.channels == C::Rgba {
-        return match color.precision {
-            P::U8 => resize_typed::<StraightAlpha<[u8; 4]>>(args),
-            P::U16 => resize_typed::<StraightAlpha<[u16; 4]>>(args),
-            P::F32 => resize_typed::<StraightAlpha<[f32; 4]>>(args),
-        };
+        if straight_alpha && color.channels == C::Rgba {
+            return match color.precision {
+                P::U8 => resize_typed::<StraightAlpha<[u8; 4], G>>(args),
+                P::U16 => resize_typed::<StraightAlpha<[u16; 4], G>>(args),
+                P::F32 => resize_typed::<StraightAlpha<[f32; 4], G>>(args),
+            };
+        }
+
+        match (color.precision, color.channels) {
+            (P::U8, C::Alpha | C::Grayscale) => resize_typed::<Pixel<[u8; 1], G>>(args),
+            (P::U16, C::Alpha | C::Grayscale) => resize_typed::<Pixel<[u16; 1], G>>(args),
+            (P::F32, C::Alpha | C::Grayscale) => resize_typed::<Pixel<[f32; 1], G>>(args),
+            (P::U8, C::Rgb) => resize_typed::<Pixel<[u8; 3], G>>(args),
+            (P::U16, C::Rgb) => resize_typed::<Pixel<[u16; 3], G>>(args),
+            (P::F32, C::Rgb) => resize_typed::<Pixel<[f32; 3], G>>(args),
+            (P::U8, C::Rgba) => resize_typed::<Pixel<[u8; 4], G>>(args),
+            (P::U16, C::Rgba) => resize_typed::<Pixel<[u16; 4], G>>(args),
+            (P::F32, C::Rgba) => resize_typed::<Pixel<[f32; 4], G>>(args),
+        }
     }
 
-    match (color.precision, color.channels) {
-        (P::U8, C::Alpha | C::Grayscale) => resize_typed::<Pixel<[u8; 1]>>(args),
-        (P::U16, C::Alpha | C::Grayscale) => resize_typed::<Pixel<[u16; 1]>>(args),
-        (P::F32, C::Alpha | C::Grayscale) => resize_typed::<Pixel<[f32; 1]>>(args),
-        (P::U8, C::Rgb) => resize_typed::<Pixel<[u8; 3]>>(args),
-        (P::U16, C::Rgb) => resize_typed::<Pixel<[u16; 3]>>(args),
-        (P::F32, C::Rgb) => resize_typed::<Pixel<[f32; 3]>>(args),
-        (P::U8, C::Rgba) => resize_typed::<Pixel<[u8; 4]>>(args),
-        (P::U16, C::Rgba) => resize_typed::<Pixel<[u16; 4]>>(args),
-        (P::F32, C::Rgba) => resize_typed::<Pixel<[f32; 4]>>(args),
+    if gamma_correction && color.channels != Channels::Alpha {
+        dispatch::<ApplyGamma>(args, color, straight_alpha);
+    } else {
+        dispatch::<IgnoreGamma>(args, color, straight_alpha);
     }
 }
 
@@ -270,18 +295,59 @@ fn to_resize_filter_type(filter: ResizeFilter) -> resize::Type {
     }
 }
 
-use pixel::{Pixel, StraightAlpha};
+use pixel::{ApplyGamma, GammaCorrection, IgnoreGamma, Pixel, StraightAlpha};
 mod pixel {
     use glam::Vec4;
 
-    pub(crate) struct Pixel<T> {
-        _marker: std::marker::PhantomData<T>,
+    pub(crate) struct Pixel<T, Gamma> {
+        _marker: std::marker::PhantomData<(T, Gamma)>,
     }
-    impl<T> Default for Pixel<T> {
+    impl<T, Gamma> Default for Pixel<T, Gamma> {
         fn default() -> Self {
             Self {
                 _marker: std::marker::PhantomData,
             }
+        }
+    }
+
+    pub(crate) struct IgnoreGamma;
+    pub(crate) struct ApplyGamma;
+    pub(crate) trait GammaCorrection: Send + Sync {
+        fn to_linear_vec4(input: Vec4) -> Vec4 {
+            input
+        }
+        fn to_linear_f32(input: f32) -> f32 {
+            input
+        }
+
+        fn from_linear_vec4(input: Vec4) -> Vec4 {
+            input
+        }
+        fn from_linear_f32(input: f32) -> f32 {
+            input
+        }
+    }
+    impl GammaCorrection for IgnoreGamma {}
+    // Instead of a proper sRGB <-> linear RGB conversion or gamma 2.2, we use gamma 2.0 here.
+    // This is visually very similar to gamma 2.2 but a lot faster to compute.
+    impl GammaCorrection for ApplyGamma {
+        fn to_linear_vec4(input: Vec4) -> Vec4 {
+            let mut out = input * input;
+            out.w = input.w; // don't apply gamma correction to alpha channel
+            out
+        }
+        fn to_linear_f32(input: f32) -> f32 {
+            input * input
+        }
+
+        fn from_linear_vec4(input: Vec4) -> Vec4 {
+            // TODO: Hope and pray that this mess gets optimized to a sqrt_ps instruction by the compiler
+            let mut out = input.max(Vec4::ZERO).powf(0.5);
+            out.w = input.w;
+            out
+        }
+        fn from_linear_f32(input: f32) -> f32 {
+            input.max(0.0).sqrt()
         }
     }
 
@@ -321,99 +387,112 @@ mod pixel {
     }
 
     pub(crate) trait IntoAccumulator<T> {
-        fn to_accumulator(self) -> T;
-        fn to_value(acc: T) -> Self;
+        fn to_accumulator<G: GammaCorrection>(self) -> T;
+        fn to_value<G: GammaCorrection>(acc: T) -> Self;
     }
     impl IntoAccumulator<f32> for [u8; 1] {
-        fn to_accumulator(self) -> f32 {
-            self[0] as f32
+        fn to_accumulator<G: GammaCorrection>(self) -> f32 {
+            G::to_linear_f32(self[0] as f32)
         }
-        fn to_value(acc: f32) -> Self {
-            [(acc + 0.5) as u8]
+        fn to_value<G: GammaCorrection>(acc: f32) -> Self {
+            [(G::from_linear_f32(acc) + 0.5) as u8]
         }
     }
     impl IntoAccumulator<f32> for [u16; 1] {
-        fn to_accumulator(self) -> f32 {
-            self[0] as f32
+        fn to_accumulator<G: GammaCorrection>(self) -> f32 {
+            G::to_linear_f32(self[0] as f32)
         }
-        fn to_value(acc: f32) -> Self {
-            [(acc + 0.5) as u16]
+        fn to_value<G: GammaCorrection>(acc: f32) -> Self {
+            [(G::from_linear_f32(acc) + 0.5) as u16]
         }
     }
     impl IntoAccumulator<f32> for [f32; 1] {
-        fn to_accumulator(self) -> f32 {
-            self[0]
+        fn to_accumulator<G: GammaCorrection>(self) -> f32 {
+            G::to_linear_f32(self[0])
         }
-        fn to_value(acc: f32) -> Self {
-            [acc]
+        fn to_value<G: GammaCorrection>(acc: f32) -> Self {
+            [G::from_linear_f32(acc)]
         }
     }
     impl IntoAccumulator<Vec4> for [u8; 3] {
-        fn to_accumulator(self) -> Vec4 {
-            Vec4::new(self[0] as f32, self[1] as f32, self[2] as f32, 0.0)
+        fn to_accumulator<G: GammaCorrection>(self) -> Vec4 {
+            G::to_linear_vec4(Vec4::new(
+                self[0] as f32,
+                self[1] as f32,
+                self[2] as f32,
+                0.0,
+            ))
         }
-        fn to_value(acc: Vec4) -> Self {
-            let out = acc + 0.5;
+        fn to_value<G: GammaCorrection>(acc: Vec4) -> Self {
+            let out = G::from_linear_vec4(acc) + 0.5;
             [out.x as u8, out.y as u8, out.z as u8]
         }
     }
     impl IntoAccumulator<Vec4> for [u16; 3] {
-        fn to_accumulator(self) -> Vec4 {
-            Vec4::new(self[0] as f32, self[1] as f32, self[2] as f32, 0.0)
+        fn to_accumulator<G: GammaCorrection>(self) -> Vec4 {
+            G::to_linear_vec4(Vec4::new(
+                self[0] as f32,
+                self[1] as f32,
+                self[2] as f32,
+                0.0,
+            ))
         }
-        fn to_value(acc: Vec4) -> Self {
-            let out = acc + 0.5;
+        fn to_value<G: GammaCorrection>(acc: Vec4) -> Self {
+            let out = G::from_linear_vec4(acc) + 0.5;
             [out.x as u16, out.y as u16, out.z as u16]
         }
     }
     impl IntoAccumulator<Vec4> for [f32; 3] {
-        fn to_accumulator(self) -> Vec4 {
-            Vec4::new(self[0], self[1], self[2], 0.0)
+        fn to_accumulator<G: GammaCorrection>(self) -> Vec4 {
+            G::to_linear_vec4(Vec4::new(self[0], self[1], self[2], 0.0))
         }
-        fn to_value(acc: Vec4) -> Self {
-            [acc.x, acc.y, acc.z]
+        fn to_value<G: GammaCorrection>(acc: Vec4) -> Self {
+            let out = G::from_linear_vec4(acc);
+            [out.x, out.y, out.z]
         }
     }
     impl IntoAccumulator<Vec4> for [u8; 4] {
-        fn to_accumulator(self) -> Vec4 {
-            Vec4::new(
+        fn to_accumulator<G: GammaCorrection>(self) -> Vec4 {
+            G::to_linear_vec4(Vec4::new(
                 self[0] as f32,
                 self[1] as f32,
                 self[2] as f32,
                 self[3] as f32,
-            )
+            ))
         }
-        fn to_value(acc: Vec4) -> Self {
-            let out = acc + 0.5;
+        fn to_value<G: GammaCorrection>(acc: Vec4) -> Self {
+            let out = G::from_linear_vec4(acc) + 0.5;
             [out.x as u8, out.y as u8, out.z as u8, out.w as u8]
         }
     }
     impl IntoAccumulator<Vec4> for [u16; 4] {
-        fn to_accumulator(self) -> Vec4 {
-            Vec4::new(
+        fn to_accumulator<G: GammaCorrection>(self) -> Vec4 {
+            G::to_linear_vec4(Vec4::new(
                 self[0] as f32,
                 self[1] as f32,
                 self[2] as f32,
                 self[3] as f32,
-            )
+            ))
         }
-        fn to_value(acc: Vec4) -> Self {
-            let out = acc + 0.5;
+        fn to_value<G: GammaCorrection>(acc: Vec4) -> Self {
+            let out = G::from_linear_vec4(acc) + 0.5;
             [out.x as u16, out.y as u16, out.z as u16, out.w as u16]
         }
     }
     impl IntoAccumulator<Vec4> for [f32; 4] {
-        fn to_accumulator(self) -> Vec4 {
-            Vec4::new(self[0], self[1], self[2], self[3])
+        fn to_accumulator<G: GammaCorrection>(self) -> Vec4 {
+            G::to_linear_vec4(Vec4::new(self[0], self[1], self[2], self[3]))
         }
-        fn to_value(acc: Vec4) -> Self {
-            [acc.x, acc.y, acc.z, acc.w]
+        fn to_value<G: GammaCorrection>(acc: Vec4) -> Self {
+            let out = G::from_linear_vec4(acc);
+            [out.x, out.y, out.z, out.w]
         }
     }
 
-    impl<T, const N: usize> resize::PixelFormat for Pixel<[T; N]>
+    impl<T, G, const N: usize> resize::PixelFormat for Pixel<[T; N], G>
     where
         T: Send + Sync + Copy,
+        G: GammaCorrection,
         [T; N]: Default,
         Selector<N>: SelectAccumulator,
         [T; N]: IntoAccumulator<<Selector<N> as SelectAccumulator>::Accumulator>,
@@ -429,7 +508,7 @@ mod pixel {
         }
 
         fn add(&self, acc: &mut Self::Accumulator, inp: Self::InputPixel, coeff: f32) {
-            acc.add_scaled(inp.to_accumulator(), coeff);
+            acc.add_scaled(inp.to_accumulator::<G>(), coeff);
         }
 
         fn add_acc(acc: &mut Self::Accumulator, inp: Self::Accumulator, coeff: f32) {
@@ -437,14 +516,14 @@ mod pixel {
         }
 
         fn into_pixel(&self, acc: Self::Accumulator) -> Self::OutputPixel {
-            Self::OutputPixel::to_value(acc)
+            Self::OutputPixel::to_value::<G>(acc)
         }
     }
 
-    pub(crate) struct StraightAlpha<T> {
-        _marker: std::marker::PhantomData<T>,
+    pub(crate) struct StraightAlpha<T, G> {
+        _marker: std::marker::PhantomData<(T, G)>,
     }
-    impl<T> Default for StraightAlpha<T> {
+    impl<T, G> Default for StraightAlpha<T, G> {
         fn default() -> Self {
             Self {
                 _marker: std::marker::PhantomData,
@@ -452,70 +531,64 @@ mod pixel {
         }
     }
     pub(crate) trait IntoStraightAlphaAccumulator<T> {
-        fn to_accumulator(self) -> T;
-        fn to_value(acc: T) -> Self;
+        fn to_accumulator<G: GammaCorrection>(self) -> T;
+        fn to_value<G: GammaCorrection>(acc: T) -> Self;
     }
+    const ALPHA_EPSILON: f32 = 0.01;
     impl IntoStraightAlphaAccumulator<Vec4> for [u8; 4] {
-        fn to_accumulator(self) -> Vec4 {
-            let v = Vec4::new(
+        fn to_accumulator<G: GammaCorrection>(self) -> Vec4 {
+            let v = G::to_linear_vec4(Vec4::new(
                 self[0] as f32,
                 self[1] as f32,
                 self[2] as f32,
                 self[3] as f32,
-            );
+            ));
             v * Vec4::new(v.w, v.w, v.w, 1.0)
         }
-        fn to_value(acc: Vec4) -> Self {
+        fn to_value<G: GammaCorrection>(acc: Vec4) -> Self {
             let a = acc.w;
-            let a_r = if a < (0.5 / 255.0) { 0.0 } else { a.recip() };
+            let a_r = if a <= ALPHA_EPSILON { 0.0 } else { a.recip() };
             let f = Vec4::new(a_r, a_r, a_r, 1.0);
-            let out = acc * f + 0.5;
+            let out = G::from_linear_vec4(acc * f) + 0.5;
             [out.x as u8, out.y as u8, out.z as u8, out.w as u8]
         }
     }
     impl IntoStraightAlphaAccumulator<Vec4> for [u16; 4] {
-        fn to_accumulator(self) -> Vec4 {
-            let v = Vec4::new(
+        fn to_accumulator<G: GammaCorrection>(self) -> Vec4 {
+            let v = G::to_linear_vec4(Vec4::new(
                 self[0] as f32,
                 self[1] as f32,
                 self[2] as f32,
                 self[3] as f32,
-            );
+            ));
             v * Vec4::new(v.w, v.w, v.w, 1.0)
         }
-        fn to_value(acc: Vec4) -> Self {
+        fn to_value<G: GammaCorrection>(acc: Vec4) -> Self {
             let a = acc.w;
-            let a_out = (a + 0.5) as u16;
-            if a_out == 0 {
-                return [0, 0, 0, 0];
-            }
-            let a_r = a.recip();
-            [
-                (acc.x * a_r + 0.5) as u16,
-                (acc.y * a_r + 0.5) as u16,
-                (acc.z * a_r + 0.5) as u16,
-                a_out,
-            ]
+            let a_r = if a <= ALPHA_EPSILON { 0.0 } else { a.recip() };
+            let f = Vec4::new(a_r, a_r, a_r, 1.0);
+            let out = G::from_linear_vec4(acc * f) + 0.5;
+            [out.x as u16, out.y as u16, out.z as u16, out.w as u16]
         }
     }
     impl IntoStraightAlphaAccumulator<Vec4> for [f32; 4] {
-        fn to_accumulator(self) -> Vec4 {
-            let v = Vec4::new(self[0], self[1], self[2], self[3]);
+        fn to_accumulator<G: GammaCorrection>(self) -> Vec4 {
+            let v = G::to_linear_vec4(Vec4::new(self[0], self[1], self[2], self[3]));
             v * Vec4::new(v.w, v.w, v.w, 1.0)
         }
-        fn to_value(acc: Vec4) -> Self {
+        fn to_value<G: GammaCorrection>(acc: Vec4) -> Self {
             let a = acc.w;
-            if a <= 0.0 {
-                return [0.0, 0.0, 0.0, 0.0];
-            }
-            let a_r = a.recip();
-            [acc.x * a_r, acc.y * a_r, acc.z * a_r, a]
+            let a_r = if a <= 0.0 { 0.0 } else { a.recip() };
+            let f = Vec4::new(a_r, a_r, a_r, 1.0);
+            let out = G::from_linear_vec4(acc * f).max(Vec4::ZERO);
+            [out.x, out.y, out.z, out.w]
         }
     }
 
-    impl<T> resize::PixelFormat for StraightAlpha<[T; 4]>
+    impl<T, G> resize::PixelFormat for StraightAlpha<[T; 4], G>
     where
         T: Send + Sync + Copy,
+        G: GammaCorrection,
         [T; 4]: Default + IntoStraightAlphaAccumulator<Vec4>,
     {
         type InputPixel = [T; 4];
@@ -529,7 +602,7 @@ mod pixel {
         }
 
         fn add(&self, acc: &mut Self::Accumulator, inp: Self::InputPixel, coeff: f32) {
-            acc.add_scaled(inp.to_accumulator(), coeff);
+            acc.add_scaled(inp.to_accumulator::<G>(), coeff);
         }
 
         fn add_acc(acc: &mut Self::Accumulator, inp: Self::Accumulator, coeff: f32) {
@@ -537,7 +610,7 @@ mod pixel {
         }
 
         fn into_pixel(&self, acc: Self::Accumulator) -> Self::OutputPixel {
-            Self::OutputPixel::to_value(acc)
+            Self::OutputPixel::to_value::<G>(acc)
         }
     }
 }
